f18eec5c3c05ee90daba1fa6ccaeb4280679cc9a
Bijay kumar shah <bjsah3277@gmail.com>
2026-02-27 11:19:21 +0530

    small changes

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import express from 'express';
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json("Hello World for Github Action CI/CD")
+    res.json({ "message": "Hello World for Github Action using CI/CD pipeline" })
 })
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
+
+// npx gitignore node ---> to create .gitignore file for node projects
